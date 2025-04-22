434fb7429e2aec81cd89390d81cae245c02b8794
j-ootake <j-ootake@cseinfo.co.jp>
2025-04-22 15:21:20 +0900

    requestの拡張実装を変更

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -1,9 +1,12 @@
-import { UserInfo } from '../UserInfo'
+import { users } from '../../drizzle/schema'
+import { SelectModel } from 'drizzle-orm'
+
+type User = SelectModel<typeof users>
 
 declare global {
   namespace Express {
     export interface Request {
-      user?: UserInfo
+      user?: User
     }
   }
 }
